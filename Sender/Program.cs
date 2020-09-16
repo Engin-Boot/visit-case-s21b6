d2de9f9665a87410f6987dcf3b2ce1cec6b340f0
Vadhnani <Sonia.Vadhnani@philips.com>
2020-09-16 17:19:35 +0530

    Sender added with sln

---
+++ Sender/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Sender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ReadCsvFileAndExtarctDateAndTime object1 = new ReadCsvFileAndExtarctDateAndTime();
+            List<ReadCsvFileAndExtarctDateAndTime.DateAndTime> dateAndTimeList;
+            dateAndTimeList = object1.extractDateAndTime();
+
+            foreach (var entry in dateAndTimeList)
+            {
+
+                Console.Write(entry.Date.Year + " ");
+                Console.Write(entry.Date.Month + " ");
+                Console.Write(entry.Date.Day + " ");
+                Console.Write(entry.Time.Hours + " ");
+                Console.Write(entry.Time.Minutes + " ");
+                Console.WriteLine(entry.Time.Seconds);
+            }
+
+        
+
+        }
+    }
+}
---
+++ Sender/ReadCsvFile.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization.Configuration;
+
+namespace Sender
+{
+     class ReadCsvFileAndExtarctDateAndTime
+    {
+         public class DateAndTime
+        {
+            public DateTime Date { get; set; }
+            public TimeSpan Time { get; set; }
+            
+        }
+        
+        public List<DateAndTime> extractDateAndTime()
+        {
+            
+            string formatForDate = "dd/MM/yyyy";
+            string formatForTime = "HH:mm:ss";
+
+        
+
+            CultureInfo provider = new CultureInfo("en-GB");
+            Thread.CurrentThread.CurrentCulture = provider;
+
+
+            
+            List<DateAndTime> allDatesandTime = new List<DateAndTime>();
+            String csvInputFilePath = Directory.GetCurrentDirectory();
+            String csvFileName = "FootFallEntries.csv";
+            csvInputFilePath += @"\" + csvFileName;
+            string line;
+            try
+            {
+                StreamReader dataInCsvFile = new StreamReader(csvInputFilePath);
+                dataInCsvFile.ReadLine();
+                while(!dataInCsvFile.EndOfStream)
+                {
+                    line= dataInCsvFile.ReadLine();
+                    string[] eachRowOfCsvFile = line.Split(',');
+                    DateAndTime objOfDateAndTime = new DateAndTime();
+
+                    try
+                    {
+                        objOfDateAndTime.Date = DateTime.ParseExact(eachRowOfCsvFile[0], formatForDate, provider);
+                        
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("{0} is not in the correct format.", eachRowOfCsvFile[0]);
+                    }
+                    try
+                    {
+                        objOfDateAndTime.Time = TimeSpan.Parse(eachRowOfCsvFile[1]);
+                        
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("{0} is not in the correct format.", eachRowOfCsvFile[1]);
+                    }
+                    
+
+                    allDatesandTime.Add(objOfDateAndTime);
+                }
+
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception + "FILE NOT FOUND");
+                throw;
+            }
+
+            return allDatesandTime;
+         }
+    }
+}
