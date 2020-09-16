f9748126f1c67c52ba1816538d18cc945cae0646
venkatesh-cs <venkatesh_cs@outlook.com>
2020-09-16 12:02:15 +0530

    Adding Communication Primitive

---
+++ Receiver/Receiver/CommPrimitive.cs
@@ -0,0 +1,16 @@
+﻿using System;
+    //public enum RegionInHosp
+    //{
+    //    Entrance1, Entrance2, Entrance3,Entrance4
+    //};
+    class CommPrimitive
+    {
+        public DateTime DateAndTime = new DateTime(2000,00,00);
+        public int Counter
+        {
+            get;
+            set;
+        }
+    //public string RegionToEnum { set; get; }
+    }
+
