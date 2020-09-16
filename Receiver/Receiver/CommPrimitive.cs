def719e64d96db5c58d11526d7eb84b2d6cbc471
venkatesh-cs <venkatesh_cs@outlook.com>
2020-09-16 15:11:08 +0530

    Updating Communication Primitive

--- Receiver/Receiver/CommPrimitive.cs
+++ Receiver/Receiver/CommPrimitive.cs
@@ -1,16 +1,22 @@
 ﻿using System;
-    //public enum RegionInHosp
-    //{
-    //    Entrance1, Entrance2, Entrance3,Entrance4
-    //};
-    class CommPrimitive
+//public enum RegionInHosp
+//{
+//    Entrance1, Entrance2, Entrance3,Entrance4
+//};
+namespace Receiver
+{
+    internal class CommPrimitive
     {
-        public DateTime DateAndTime = new DateTime(2000,00,00);
-        public int Counter
+        public DateTime Date { get; set; }
+        public TimeSpan time { get; set; }
+
+        public const int Counter = 1;
+        public CommPrimitive(int year, int month, int day, int hour, int min, int sec)
         {
-            get;
-            set;
+            Date = new DateTime(year, month, day);
+            time = new TimeSpan(hour, min, sec);
         }
-    //public string RegionToEnum { set; get; }
     }
+}
+
 
