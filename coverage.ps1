7aabd5b4e3119b43254ecf294546f9be9954825b
SoniaVadhnani <68679226+SoniaVadhnani@users.noreply.github.com>
2020-09-22 18:09:48 +0530

    Update coverage.ps1

--- coverage.ps1
+++ coverage.ps1
@@ -11,20 +11,20 @@ function WriteXmlToScreen ([xml]$xml)
     Write-Output $StringWriter.ToString();
 }
 
-$report = Get-Content -Path UnitTestProject\TestResults\*\coverage.cobertura.xml | Out-String
+$report = Get-Content -Path Sender.Test\TestResults\*\coverage.cobertura.xml | Out-String
 Write-Host "---------------------------------"
-Write-Host "Code Coverage report ..." 
+Write-Host "Code Coverage report of SenderTest..." 
 Write-Host "---------------------------------"
-WriteXmlToScreen $report
+WriteXmlToScreen $report_of_sender
 
-[xml]$doc = $report
+[xml]$doc_sender = $report_of_sender
 
 Write-Host ""
 Write-Host "---------------------------------"
-Write-Host "Code Coverage report Analysis..." 
+Write-Host "Code Coverage report of SenderTest Analysis..." 
 Write-Host "---------------------------------"
 
-$result = 0
+$result_sender = 0
 
 Write-Host "Line-Coverage: ["$doc.coverage.'line-rate'"] Branch-Coverage: ["$doc.coverage.'branch-rate'"]"
 Write-Host ""
@@ -32,14 +32,40 @@ Write-Host ""
     Write-Host "Package:"  $pkg.name "Line-Coverage:"$pkg.'line-rate'
 
     if($pkg.'line-rate' -lt $linerate){
-        $result= 1
+        $result_sender= 1
        }
     }
 
-if($result -eq 1){
+$report_of_receiver = Get-Content -Path  Receiver.Test\TestResults\*\coverage.cobertura.xml | Out-String
+Write-Host "---------------------------------"
+Write-Host "Code Coverage report of ReceiverTest ..." 
+Write-Host "---------------------------------"
+WriteXmlToScreen $report_of_receiver
+
+[xml]$doc_receiver = $report_of_receiver
+
+Write-Host ""
+Write-Host "---------------------------------"
+Write-Host "Code Coverage report of ReceiverTest Analysis..." 
+Write-Host "---------------------------------"
+
+$result_receiver = 0
+
+Write-Host "Line-Coverage: ["$doc_receiver.coverage.'line-rate'"] Branch-Coverage: ["$doc_receiver.coverage.'branch-rate'"]"
+Write-Host ""
+ foreach ($pkg in $doc_receiver.coverage.packages.package) {
+    Write-Host "Package:"  $pkg.name "Line-Coverage:"$pkg.'line-rate'
+
+    if($pkg.'line-rate' -lt $linerate){
+        $result_receiver= 1
+       }
+    }
+
+if(($result_sender -eq 1) -or ($result_receiver -eq 1)){
     Write-Host "Coverage Check: failed" -ForegroundColor red 
+    exit 1
 }
 else{
     Write-Host "Coverage Check: Passed" -ForegroundColor green 
+    exit 0
 }
-exit $result
